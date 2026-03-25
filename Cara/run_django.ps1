0e7a3f3c31729aebc9742cecdcbce41eeefe3e00
Faaiz Gul <faaizgul222@gmail.com>
2026-03-26 01:56:23 +0500

    project start with 1 click

---
+++ Cara/run_django.ps1
@@ -0,0 +1,11 @@
+# Navigate to project folder
+Set-Location -Path "C:\Users\faaiz\Cara Collections\Cara"
+
+# Activate virtual environment
+& "C:\Users\faaiz\Cara Collections\venv\Scripts\Activate.ps1"
+
+# Start Django server in a new window
+Start-Process python -ArgumentList "manage.py runserver 8005" -WindowStyle Hidden
+
+# Open browser automatically
+Start-Process "http://127.0.0.1:8005"
