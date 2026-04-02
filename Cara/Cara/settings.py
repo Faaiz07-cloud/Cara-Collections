cdcc1c10adc0a49104996d09d35853a86c3be4ae
Faaiz Gul <faaizgul222@gmail.com>
2026-04-02 14:40:26 +0500

    minor changes in settings.py

--- Cara/Cara/settings.py
+++ Cara/Cara/settings.py
@@ -122,9 +122,13 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'), 
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- Cara/run_django.ps1
+++ Cara/run_django.ps1
@@ -5,7 +5,7 @@ Set-Location -Path "C:\Users\faaiz\Cara Collections\Cara"
 & "C:\Users\faaiz\Cara Collections\venv\Scripts\Activate.ps1"
 
 # Start Django server in a new window
-Start-Process python -ArgumentList "manage.py runserver 1521" -WindowStyle Hidden
+Start-Process python -ArgumentList "manage.py runserver 8888" -WindowStyle Hidden
 
 # Open browser automatically
-Start-Process "http://127.0.0.1:1521"
+Start-Process "http://127.0.0.1:8888"
