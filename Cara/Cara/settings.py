fbf0fe5a3179928d34facf1874f765074c2fded2
Faaiz Gul <faaizgul222@gmail.com>
2026-04-02 14:21:34 +0500

    making website fully responsive

--- Cara/Cara/settings.py
+++ Cara/Cara/settings.py
@@ -118,15 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+import os 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'     
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
